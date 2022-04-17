b10b938148596a1fe7ed5aa1cc02911caf7f0f64
Maksim Androfagin <androfagin@mail.ru>
2022-04-17 23:07:51 +0400

    Create new project ClientServerAppHosts

---
+++ src/Server.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 8088;
+        ServerSocket serverSocket = new ServerSocket(port);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("New connection accepted");
+            out.println(String.format("What is your name?"));
+            final String name = in.readLine();
+            out.println(String.format("Hi %s! Are you child? (yes/no)", name));
+            String answer = in.readLine();
+            if (answer.equals("yes")) {
+                out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+            } else {
+                out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+            }
+        }
+    }
+}
