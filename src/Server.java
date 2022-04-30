4cad0fcdb762047b29549b07bfba3e130743ffcf
Maksim Androfagin <androfagin@mail.ru>
2022-04-30 11:20:13 +0400

    modified file

--- src/Server.java
+++ src/Server.java
@@ -10,22 +10,22 @@ public class Server {
         int port = 8088;
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                System.out.println("New connection accepted");
-                out.println(String.format("What is your name?"));
-                final String name = in.readLine();
-                out.println(String.format("Hi %s! Are you child? (yes/no)", name));
-                String answer = in.readLine();
-                if (answer.equals("yes")) {
-                    out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
-                } else {
-                    out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    System.out.println("New connection accepted");
+                    out.println(String.format("What is your name?"));
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s! Are you child? (yes/no)", name));
+                    String answer = in.readLine();
+                    if (answer.equals("yes")) {
+                        out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+                    } else {
+                        out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                clientSocket.close();
-                out.close();
-                in.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
